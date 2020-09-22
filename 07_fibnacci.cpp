d187c34d8e4cc86a32d7b87500085f905c9c1bf6
ubaidh <ubaidh@live.com>
2020-09-23 01:06:07 +0530

    update

--- 07_fibnacci.cpp
+++ 07_fibnacci.cpp
@@ -1,16 +1,13 @@
 #include<iostream>
-long long g[40]={0};
 int fib(int n){
-	if(g[n]!=0)return g[n];
     if(n<2){
         return n;
     }else{
-    	g[n]=fib(n-2)+fib(n-1);
-        return g[n];
+        return fib(n-2)+fib(n-1);
     }
 }
 int main(){
-    for(int i=0;i<=40;i++){
+    for(int i=0;i<=10;i++){
         std::cout<<fib(i)<<" ";
     }
 
