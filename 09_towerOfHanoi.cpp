dde06ab1c7a6b2bdf04ba0a301576858b1a16f24
ubaidh <38114742+ubaidh@users.noreply.github.com>
2020-09-23 01:15:43 +0530

    Update 09_towerOfHanoi.cpp

---
+++ 09_towerOfHanoi.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+void TOH(int n,int a,int b,int c){ 
+    if(n>0){
+        TOH(n-1,a,c,b);
+        printf("%d%d\n",a,c);
+        TOH(n-1,b,a,c);
+    } 
+}
+int main(){
+    TOH(10,1,2,3);
+    return 0;
+}
