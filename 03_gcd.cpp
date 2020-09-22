de3a5dff464fc5203faf6f93cd185aa84b2db948
ubaidh <ubaidh@live.com>
2020-09-22 22:05:14 +0530

    gcd

---
+++ 03_gcd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int gcd(int x, int y){
+  if (y == 0) return x;
+  else return gcd(y, x % y);
+}
+int main(){
+   int a,b;
+   std::cout<<"enter a and b"<<std::endl;
+   std::cin>>a>>b;
+   std::cout<<gcd(a,b);
+   return 0;
+}
